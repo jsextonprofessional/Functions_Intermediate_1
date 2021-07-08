d3699bf0a2b245ac7027c3786d6284b91cc56de8
jsprofessional <jshuman@protonmail.com>
2021-07-08 17:26:24 -0500

    all but 4

--- Functions_Intermediate.py
+++ Functions_Intermediate.py
@@ -1,8 +1,8 @@
 # 1. Update Values in Dictionaries and Lists
 x = [ [5,2,3], [10,8,9] ]
 students = [
-     {'first_name':  'Michael', 'last_name' : 'Jordan'},
-     {'first_name' : 'John', 'last_name' : 'Rosales'}
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
 ]
 sports_directory = {
     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
@@ -10,69 +10,70 @@ sports_directory = {
 }
 z = [ {'x': 10, 'y': 20} ]
 
-# 1. Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+# 1.1 Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
 x[1][0] = 15
 print(x)
 
-# 2. Change the last_name of the first student from 'Jordan' to 'Bryant'
+# 1.2 Change the last_name of the first student from 'Jordan' to 'Bryant'
 students[0]['last_name'] = 'Bryant'
 print(students)
 
-# 3. In the sports_directory, change 'Messi' to 'Andres'
+# 1.3 In the sports_directory, change 'Messi' to 'Andres'
 sports_directory['soccer'][0] = 'Andres'
 print(sports_directory)
 
-# 4. Change the value 20 in z to 30
+# 1.4 Change the value 20 in z to 30
 z[0]['y'] = 30
 print(z)
 
 
-
 # 2. Iterate Through a List of Dictionaries
-# Create a function iterateDictionary(some_list) that, given a list of dictionaries, the function loops through each dictionary in the list and prints each key and the associated value. For example, given the following list:
 students2 = [
-         {'first_name':  'Michael', 'last_name' : 'Jordan'},
-         {'first_name' : 'John', 'last_name' : 'Rosales'},
-         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
+
 def iterateDictionary(students2):
     for i in range(0, len(students2)):
-        print(students2[i])
+        output = ""
+        for key, val in students2[i].items():
+            output += f"{key} - {val}, "
+        print(output)
+
 iterateDictionary(students2)
-# should output: (it's okay if each key-value pair ends up on 2 separate lines;
-# bonus to get them to appear exactly as below!)
-# first_name - Michael, last_name - Jordan
-# first_name - John, last_name - Rosales
-# first_name - Mark, last_name - Guillen
-# first_name - KB, last_name - Tonel
 
 
 # 3. Get Values From a List of Dictionaries
-# Create a function iterateDictionary2(key_name, some_list) that, given a list of dictionaries and a key name, the function prints the value stored in that key for each dictionary. For example, iterateDictionary2('first_name', students) should output:
+def iterateDictionary2(key_name, some_list):
+    for j in range(0, len(some_list)):
+        for key2, val2 in some_list[j].items():
+            if key2 == key_name:
+                print(val2)
 
-# Michael
-# John
-# Mark
-# KB
-
-# And iterateDictionary2('last_name', students) should output:
-
-# Jordan
-# Rosales
-# Guillen
-# Tonel
-
+iterateDictionary2('first_name', students2)
+iterateDictionary2('last_name', students2)
 
 
 # 4. Iterate Through a Dictionary with List Values
 # Create a function printInfo(some_dict) that given a dictionary whose values are all lists, prints the name of each key along with the size of its list, and then prints the associated values within each key's list. For example:
+def printInfo(some_dict):
+# print name of each key
+    for k in range(0, len(some_dict)):
+        for key3, val3 in some_dict[k].items():
+            print f""
 
-# dojo = {
-#    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
-#    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
-# }
-# printInfo(dojo)
+
+
+# print the size of each list
+# print each key's value on own line
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
 # # output:
 # 7 LOCATIONS
 # San Jose
